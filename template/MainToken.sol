7d7060af6e22fc34c6cb0c7813efb94115086ed3
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-12-17 16:08:24 +0300

    remove erc223

--- template/MainToken.sol
+++ template/MainToken.sol
@@ -3,18 +3,11 @@ pragma solidity ^0.4.23;
 import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
 import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
-//#if "D_ERC" == "ERC223"
-import "sc-library/contracts/ERC223/ERC223MintableToken.sol";
-//#endif
 import "./FreezableMintableToken.sol";
 import "./Consts.sol";
 
 
-contract MainToken is Consts, FreezableMintableToken, BurnableToken, Pausable
-    //#if "D_ERC" == "ERC223"
-    , ERC223MintableToken
-    //#endif
-{
+contract MainToken is Consts, FreezableMintableToken, BurnableToken, Pausable {
     event Initialized();
     bool public initialized = false;
 
