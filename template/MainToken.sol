3972073083fedc7a950e9ecf3f384c46f764cdc7
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2020-09-29 17:12:07 +0300

    contracts to 10 files

--- template/MainToken.sol
+++ template/MainToken.sol
@@ -4,7 +4,19 @@ import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
 import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 import "./FreezableMintableToken.sol";
-import "./Consts.sol";
+// import "./Consts.sol";
+
+contract Consts {
+    uint public constant TOKEN_DECIMALS = D_DECIMALS;
+    uint8 public constant TOKEN_DECIMALS_UINT8 = D_DECIMALS;
+    uint public constant TOKEN_DECIMAL_MULTIPLIER = 10 ** TOKEN_DECIMALS;
+
+    string public constant TOKEN_NAME = "D_NAME";
+    string public constant TOKEN_SYMBOL = "D_SYMBOL";
+    bool public constant PAUSED = D_PAUSE_TOKENS;
+    address public constant TARGET_USER = D_CONTRACTS_OWNER;
+    bool public constant CONTINUE_MINTING = D_CONTINUE_MINTING;
+}
 
 
 contract MainToken is Consts, FreezableMintableToken, BurnableToken, Pausable {
